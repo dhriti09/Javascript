7a363aa1303abfa3f03d2cbdd66fdd4c224b2ded
Dhriti Yadav <DHRITIYADAV0919@GMAIL.COM>
2026-03-10 17:43:24 +0000

    container config done

---
+++ hello/variables.js
@@ -0,0 +1,15 @@
+const accountId = 7676
+let accountEmail ="abc@gmail.com"
+var acountPassword = "12345"
+accountCity ="Jaipur"
+let accountState
+
+// accountId =  2 //node js will analyse
+accountEmail ="fgfg@gmail.com"
+accounPassword = 67678
+accountCity = "Bengaluru"
+
+console.log(accountId);
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+console.table([accounPassword,accountEmail,accountCity,accountState])
+                                                                                                                                                                                                                                                                                                                                                                                            
