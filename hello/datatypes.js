1251709ab9fcac097383a38187ecab0d8c4b7229
Dhriti Yadav <DHRITIYADAV0919@GMAIL.COM>
2026-03-12 14:49:23 +0000

    container config done

---
+++ hello/datatypes.js
@@ -0,0 +1,33 @@
+"use strict";//treate  all js code as newer js  or version ....and there is no way to  reverse it ...as there automatically everything runs in advance 
+//alert("3+3")//in  this arithematic operation also run but if we run it in node it will give you error ..it only runs in html and console  and here it is document 
+//there is another syntax to use alert 
+
+//when to use spacing or no 
+
+//in this we avoid semicolon in every js
+ console.log(3+3) 
+ console.log("dhriti") //avoid semicolon for readability higher
+
+
+
+ //not much datatypes in javascripts
+
+ let name ="dhriti" //string datatype
+ let age = 23 //number
+
+ let isLoggedin //boolean
+
+
+ //range of nuber => 2 to power  53 ,,if number bigger use INT
+ // string  => use double  quote 
+ //boolean either true or false like 0 or 1
+ //null  -stand alone value  it is representation of emply value 
+ ///like if  we want to know temperature if it cant send it will be used(null)
+ //undefined - when we not define value it will show un defined  
+
+ //symbol--implement for making componet most used in react  like used in react ..unique
+
+ //object
+//alll these above are primitvie data  type
+
+console.log(typeof "dhriti"); //shows what kind of data type is there
