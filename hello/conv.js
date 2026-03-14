78854cdde4db7f5269f78350b2d8399ece4d1246
Dhriti Yadav <DHRITIYADAV0919@GMAIL.COM>
2026-03-14 19:50:16 +0530

    Add files via upload

---
+++ hello/conv.js
@@ -0,0 +1,49 @@
+//conversion and operations
+//variable declaration
+
+let score = "33"
+
+//when in double quotes its sstring and without its number
+
+//but when it sin backend and by frontend maker the value come in form of forms
+
+console.log(typeof score);
+cosnole.log(type(score));
+
+//the capital letter used because its cpital form cause its is class based
+
+let valueNummber = Number(score)
+
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);//when abc added its NAN shows
+//when
+
+let score = null//it shows output zero
+
+let score = undefined//its shows Nan
+
+let score = true
+//gives one
+
+//if any string that cannot be convert in number its gives nan 
+let score = "dhriti"
+
+
+
+//more conversion
+//converted value in boolean
+
+let isLoggedIn = 1
+
+//pass the value in boolean 
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+//the number will now give the boolean value in the output that wheather one is true or false ... 
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNUmber);
+
