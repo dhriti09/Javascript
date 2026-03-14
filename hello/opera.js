dea2eded059c83ab9dc64de86848b315a1bf5c7b
Dhriti Yadav <DHRITIYADAV0919@GMAIL.COM>
2026-03-14 20:28:42 +0530

    operation in js

---
+++ hello/opera.js
@@ -0,0 +1,35 @@
+//operations//
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+//console.log(2 + 2);
+//console.log(2 - 2);
+//console.log(2 * 2);
+//console.log(2 ** 3);//power
+//console.log(2 / 3);
+//console.log(2 % 3);//remainder
+
+//****addition of strings  */
+
+let str1 = "dhriti"
+let str2 = "yadav"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2)//do not write this kind of code..use parenthesis
+
+//if string is first then string added if in last number added
+
+console.log(+true)//it givess one
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gaemCounter =100
+gameCounter++;//prefix and post operator
+console(gameCounter);
