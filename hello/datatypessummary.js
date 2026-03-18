faf1f6a022ffe27811975bf572128b2ad60262c6
dhriti09 <DHRITIYADAV0919@GMAIL.COM>
2026-03-18 22:56:21 +0530

    Summary

---
+++ hello/datatypessummary.js
@@ -0,0 +1,62 @@
+//call by  value
+//call be reference
+//in data storage
+//primitive basic types
+/*string 
+number 
+null
+empty
+undefined 
+symbol-for making value unique
+bigInt */
+
+//reference type or non primitive( objetcs and browser events)
+/*Array objects function */
+
+
+/*is it dynamic or static 
+id const scroe=100 we are not gounf to tell any data type so we do not define */
+
+const score: Value = 100 //number defining //tyoecript
+const scoreValue = 100.3
+const isLogedin = false;
+const OutsideTemp = null;
+let userEmail;//output undefined
+
+const id = symbol//if wanted to be unique us this
+
+console.log(id === anotherId);
+
+const bigNumber = 44545454545454n//it will automatically become bigg int
+
+
+//arrya object function
+
+const heros = ["a", "b", "c"]
+
+//can also be let myobje
+{
+    name: "dfds"
+    age:22
+}
+// variable declarion by function
+
+const myFunction = function{
+    console.log("dhriti ");
+}
+
+//how to know data type by funtion
+
+tyepof 
+//console.log(typeof bigNUmber )
+//output undefined
+
+//of null of temperature
+
+///of object
+
+
+//datatype of my function
+//output function 
+//also object function
+
