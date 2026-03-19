eca86bb1e48d05925d228342177cf2c9c6413698
dhriti09 <DHRITIYADAV0919@GMAIL.COM>
2026-03-19 09:49:15 +0530

    memoryallocation

---
+++ hello/stckmemory.js
@@ -0,0 +1,28 @@
+//how memory works in js
+//how mwmory allocation in js
+//primary memory and the secondary memory
+//stck (primitive)(it is used a copy of variable is obtained) ,heap( non-primitive)(reference of original value is obtained)
+
+
+let mywebsite = " dhriti.com "
+
+//change value
+let anotherName = "hedjed.com"
+console.log(mywebsite);
+console.log(anotherName);
+
+//here the copy is provided
+
+//advanced data type//let take user
+
+let user = {
+    email:"ddgj@gmail.com",
+    upi : "5445"
+}
+
+let user = userone 
+
+usertwo.email = "bdhbhjbdh@gmail.com"
+//we will get reference now in this in function 
+console.log(userone.email);
+console.log(usertwo.email);
