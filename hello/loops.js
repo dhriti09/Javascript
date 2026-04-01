4fd9205e61dccdc55cc17038bdc066f3eb00e100
dhriti09 <DHRITIYADAV0919@GMAIL.COM>
2026-04-01 23:37:39 +0530

    added more content

--- hello/loops.js
+++ hello/loops.js
@@ -9,6 +9,40 @@
 //kaha se jana hai kaha rukna hai ----while loop //exception aaye to ruk jana h
 
 //for me start aata h end aata h change aata hai
-for (start; end; change ) {
+//for (start; end; change) {
+    
+    //for loop ke andar utni baar code chalega jitnibaar yeh code chaleage
+    //for print 1-100
+
+    
+//}
+
+for (let i = 1; i < 101; i++){
+    console.log(i);
+}
+
+/*
+start
+while (end) {
+    change
+}*/
+
+let i = 1;
+while(i<23)
+{
+    console.log(i);
+    i++;
     
 }
+
+/*stsrt
+change 
+end  */
+let i = 12;
+do {
+    console.log(i);
+    i++;
+    
+} while (i < 20)
+    
+    //break and continue
---
+++ hello/questions.js
@@ -0,0 +1,15 @@
+//print from one to 10
+
+for (let i = 1; i < 11; i++){
+    console.log(i);
+    i++
+}
+
+//print from 10 to one while loop
+
+let i = 10;
+while (i > 0) {
+    console.log(i);
+    i--;
+    
+}
